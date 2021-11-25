bf33aba9c3fe6ae6230d1320aac5fb06ea55526d
Qqkyu <kulmisiu007@gmail.com>
2021-11-25 19:17:19 +0100

    Created initial modules

---
+++ src/modules/Details/index.tsx
@@ -0,0 +1,7 @@
+import "./detailsStyles.scss";
+
+const Details = () => {
+  return <h1>Breaking Bad Leaflet - Details</h1>;
+};
+
+export default Details;
---
+++ src/modules/Results/index.tsx
@@ -0,0 +1,7 @@
+import "./resultsStyles.scss";
+
+const Results = () => {
+  return <h1>Breaking Bad Leaflet - Results</h1>;
+};
+
+export default Results;
