3867bcc91b3001e81d54e7d59b499375895ab612
Qqkyu <kulmisiu007@gmail.com>
2021-11-28 13:33:18 +0100

    Added interfaces for api retrieved information

---
+++ src/library/common/components/Episode/episode.ts
@@ -0,0 +1,9 @@
+export interface death {
+  id: number;
+  title: string;
+  season: number;
+  episode: number;
+  air_date: string;
+  characters: string[];
+  series: string;
+}
---
+++ src/library/common/components/Quote/quote.ts
@@ -0,0 +1,6 @@
+export interface death {
+  id: number;
+  quote: string;
+  author: string;
+  series: string;
+}
